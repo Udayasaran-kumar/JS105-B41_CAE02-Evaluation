eea90e1c8231fcc84964396671af8e0e1695ab3d
Udayasaran-kumar <ssaran3091998@gmail.com>
2024-12-16 15:55:46 +0530

    Create q1.js

---
+++ q1.js
@@ -0,0 +1,29 @@
+let task=task(number){
+  title:title,
+  description:description,
+  priority:priority,
+  taskid:id,
+  task status:status
+}
+  const mypromise=new Promise((resove,reject)=>{
+   resolved{ setTimeout(()=>{
+task(input);
+    },1000)}
+    reject{
+      console.log(`${input}Rejected`);
+    }
+  })
+
+
+  let input=prompt();
+  mypromise(){
+    task(1){
+      task(2){
+        task(3){
+          task(4){
+            
+          }
+        }
+      }
+    }
+  }
